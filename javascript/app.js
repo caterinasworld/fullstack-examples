727b40b6ed1fb7fb62640304d34987cb495a0be6
Caterina Paun <caterina.paun@gmail.com>
2021-04-01 17:34:27 -0700

    Add: JavaScript examples, exercises, and solutions

---
+++ javascript/app.js
@@ -0,0 +1,31 @@
+const url = 'https://anapioficeandfire.com/api/books/';
+
+let app = document.querySelector('#books');
+let loading = document.querySelector('#loading');
+
+let addToDOM = (element, item) => {
+  let li = document.createElement(element);
+  li.innerText = `${item.name} - ${item.numberOfPages} pages`;
+  li.setAttribute('style', 'font-size: 1.2em;');
+  li.setAttribute('class', 'list-item');
+  app.append(li);
+};
+
+let fetchData = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        addToDOM('li', item);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      app.append();
+    })
+    .finally(() => {
+      app.removeChild(loading);
+    });
+};
+
+fetchData(url);
