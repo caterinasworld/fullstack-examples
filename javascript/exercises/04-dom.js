fff53b8de2c52648ca35aaea38862d62cff395f7
Caterina Paun <caterina.paun@gmail.com>
2021-04-22 17:30:03 -0700

    Update: dom example, exercise, and solution

--- javascript/exercises/04-dom.js
+++ javascript/exercises/04-dom.js
@@ -1,7 +1,17 @@
-// whenever the user enters input text, copy the user input to the output area
+// Exercise #1:
+// When the user clicks the 'copy' button, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
 
 // add an event listener on the target element
 
-// add a callback function to handle event
+// callback function to handle event
+
+// Exercise #2:
+// When the user enters input text, copy the user input to the output area
+
+// fetch JavaScript objects representing specific elements in the DOM
+
+// add an event listener on the target element
+
+// callback function to handle event
