8b4f2b7a289d260bc616f2493c37ecfce91a22cc
Caterina Paun <caterina.paun@gmail.com>
2021-05-07 17:13:37 -0700

    Update: post data example

--- express/08-post-data.js
+++ express/08-post-data.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const parser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(
-  parser.urlencoded({
-    extended: false,
-    limit: 1024,
+  express.urlencoded({
+    extended: true,
   })
 );
 
@@ -18,7 +16,7 @@ app.get('/form', (req, res) => {
   res.write('<label for="name">Name: </label>');
   res.write('<input type="text" name="name" id="name"><br />');
   res.write('<label for="email">Email: </label>');
-  res.write('<input type="text" name="email" id="email"><br />');
+  res.write('<input type="email" name="email" id="email"><br />');
   res.write('<input type="submit">');
   res.write('</form></body></html>');
   res.end();
