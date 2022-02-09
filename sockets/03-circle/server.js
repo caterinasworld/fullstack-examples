00eb95ec7650717657e18d101c4b498602fc588f
Caterina Paun <caterina.paun@gmail.com>
2022-02-08 20:11:33 -0800

    Update socket.io example 03 - circle

---
+++ sockets/03-circle/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const assert = require('assert');
+const port = 5001;
+
+app.use(express.static(__dirname + '/client/'));
+
+io.on('connection', (socket) => {
+  console.log(`client with socket id ${socket.id} connected`);
+
+  socket.on('drag', (data) => {
+    // console.log(data);
+
+    assert(data.left !== undefined);
+    assert(data.top !== undefined);
+
+    io.emit('update', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`client with socket id ${socket.id} disconnected`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Application running at "http://localhost:${port}/"`);
+});
