cb06b8c3a5b356272a6ba6ac289d938a683d6202
Caterina Paun <caterina.paun@gmail.com>
2022-10-06 16:00:14 -0700

    Update: javascript fetch example

--- javascript/02-fetch.js
+++ javascript/02-fetch.js
@@ -1,16 +1,22 @@
-console.log('test');
-
 const url = 'https://swapi.dev/api/planets/';
 
-fetch(url)
-  .then((response) => {
-    console.log(response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data.results);
-    data.results.forEach((planet) => {
-      console.log(`Planet: ${planet.name} - Population: ${planet.population}`);
-    });
-  })
-  .catch((error) => console.error(error));
+let getData = (url) => {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.next !== null && getData(data.next);
+      return data.results;
+    })
+    .then((planets) => {
+      planets.forEach((planet) => {
+        console.log(
+          `Planet: ${planet.name} - Population: ${planet.population}`
+        );
+      });
+    })
+    .catch((error) => console.error(error));
+};
+
+getData(url);
