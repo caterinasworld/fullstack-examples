4478e48b9d98c3098cbea9212fb4deab848beafa
Caterina Paun <caterina.paun@gmail.com>
2022-10-25 11:12:51 -0700

    Update: templating engine example

--- express/09-pug/app.js
+++ express/09-pug/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
