7c9149c0e35a47b891bcec8ee823c4528c53fb81
Caterina Paun <caterina.paun@gmail.com>
2022-10-25 11:00:33 -0700

    Update: middleware example

--- express/04-middleware.js
+++ express/04-middleware.js
@@ -4,13 +4,20 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 app.use((req, res, next) => {
+  console.log('========== Logging Service: URL ==========');
   console.log(req.url);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('========== Logging Service: Request Headers ==========');
+  console.log(req.headers);
   next();
 });
 
 app.get('/', (req, res, next) => {
   console.log('GET route - main');
-  res.send('This is the main route');
+  res.send('This is the main route in the 04-middleware example');
 });
 
 app.listen(port, () => {
