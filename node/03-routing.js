02129feb43d3840bb647b28d83627e9e9162fbb0
Caterina Paun <caterina.paun@gmail.com>
2022-10-25 23:57:33 -0700

    Update: routing example

--- node/03-routing.js
+++ node/03-routing.js
@@ -3,35 +3,40 @@ const data = require('./characters');
 const port = process.env.PORT || 5001;
 
 const server = http.createServer((req, res) => {
-  // console.log(characters);
-
   // main route
   if (req.method === 'GET' && req.url === '/') {
     console.log(`${req.method} - ${req.url}`);
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello! This is the main page</h1>');
+    res.write('<h1>Hello! This is the Main page</h1>');
     res.end();
   }
 
   // new route --> /about
   else if (req.method === 'GET' && req.url === '/about') {
     console.log(`${req.method} - ${req.url}`);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>This is the about page</h1>');
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Keep-Alive': 'timeout=3',
+    });
+    res.write('<h1>This is the About page</h1>');
     res.end();
   }
 
   // new route --> /contact
   else if (req.method === 'GET' && req.url === '/contact') {
     console.log(`${req.method} - ${req.url}`);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>This is the contact page</h1>');
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Set-Cookie': ['first=spongebob', 'last=squarepants'],
+    });
+    res.write('<h1>This is the Contact page</h1>');
     res.end();
   }
 
   // new route --> /data
   else if (req.method === 'GET' && req.url === '/data') {
     console.log(`${req.method} - ${req.url}`);
+    // console.log(characters);
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(data));
     res.end();
