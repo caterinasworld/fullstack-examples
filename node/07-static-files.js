9f51a8ed285e8343eb0f46434a51f3c064f30ba0
Caterina Paun <caterina.paun@gmail.com>
2022-10-27 13:05:01 -0700

    Update: static files example

--- node/07-static-files.js
+++ node/07-static-files.js
@@ -9,25 +9,19 @@ const file = new static.Server('./public');
 const server = http.createServer((req, res) => {
   // main route
   if (req.url === '/') {
-    console.log(`${req.method} - ${req.url}`);
-
     // serve index.html without specifying the file name
     file.serve(req, res);
   }
 
   // form route
   else if (req.url === '/form') {
-    console.log(`${req.method} - ${req.url}`);
-
-    // serve index.html without specifying the file name
+    // serve form.html file
     file.serveFile('/form.html', 200, {}, req, res);
   }
 
   // any other route --> handle 404
   else {
-    console.log(`${req.method} - ${req.url}`);
-
-    // server other files by specifying their names
+    // server the error.html file for any other route
     file.serveFile('/error.html', 200, {}, req, res);
   }
 });
